eea808d3c7a277be0b0e0903ff842af3c1e04560
Ethan Chan <103769713+eschan145@users.noreply.github.com>
2022-08-08 10:36:19 -0700

    Added filepaths

---
+++ file.py
@@ -0,0 +1,41 @@
+import sys
+
+
+# Get directory paths
+directory = sys.path[0]
+
+path = f"{directory}/".replace("\\", "/")
+
+image_path = f"{path}images/"
+
+
+# To be put into settings
+theme = "yellow"
+
+blank1 = f"{image_path}blank1.png"
+blank2 = f"{image_path}blank2.png"
+
+button = {
+    f"{theme}_button_normal" : f"{image_path}{theme}_button_normal.png",
+    f"{theme}_button_hover" : f"{image_path}{theme}_button_hover.png",
+    f"{theme}_button_press" : f"{image_path}{theme}_button_press.png",
+    f"{theme}_button_disable" : f"{image_path}{theme}_button_disable.png"
+}
+
+# TODO: add all of the filepaths into entry map, toggle map, etc.
+entry_normal = f"{image_path}entry_normal.png"
+entry_hover = f"{image_path}entry_hover.png"
+entry_focus = f"{image_path}entry_focus.png"
+
+toggle_true = f"{image_path}toggle_true.png"
+toggle_false = f"{image_path}toggle_false.png"
+toggle_true_hover = f"{image_path}toggle_true_hover.png"
+toggle_false_hover = f"{image_path}toggle_false_hover.png"
+
+slider_horizontal = f"{image_path}slider_horizontal.png"
+
+knob = f"{image_path}knob.png"
+
+colorchooser = f"{image_path}colorchooser.png"
+
+none = f"{image_path}application/none.png"

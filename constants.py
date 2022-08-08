08ec21874d51c0cb5a19fdb05727c83ddcd20c73
Ethan Chan <103769713+eschan145@users.noreply.github.com>
2022-08-08 10:48:13 -0700

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,36 @@
+"""Constants used by arcade-gui."""
+
+# Sides
+
+LEFT = "left"
+CENTER = "center"
+RIGHT = "right"
+
+TOP = "top"
+BOTTOM = "bottom"
+
+# Callbacks
+SINGLE = "single"
+MULTIPLE = "multiple"
+
+DISABLE_ALPHA = 160 # Alpha of disabled widget
+FOCUS_SIZE = 1.05 # [DEPRECATED]
+
+X = 0
+Y = 0
+
+ENTRY_BLINK_INTERVAL = 0.5 # Interval in seconds the caret blinks in an entry
+
+TOGGLE_VELOCITY = 2 # How fast the knob moves in a toggle
+TOGGLE_FADE = 17 # How fast the toggle fades during transitions
+
+SLIDER_VELOCITY = 10 # How fast does the slider glide
+KNOB_HOVER_SCALE = 1.1
+
+HORIZONTAL = "horizontal"
+VERTICAL = "vertical"
+
+DEFAULT_FONT_FAMILY = "Montserrat"
+DEFAULT_FONT_SIZE = 12
+
+DEFAULT_FONT = ["Montserrat", 12]
